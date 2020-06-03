f4f29773b6c92fd92f29f3cad70e2a7a1ed033e5
Trevor <tlow94@uw.edu>
2020-06-02 17:52:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+let dom = document.addEventListener("DOMContentLoaded", function(){
+document.querySelector('#text').textContent = "This is really cool!"});
+
+
+
+
+// describe( "index.js", () => {
+//     it( 'should change the text on the page when DOM content has loaded', function () {
+//       expect( document.querySelector( 'p' )
+//           .textContent )
+//         .to.equal( "JavaScript is so cool. It lets me add text to my page programmatically." );
+  
+//       let event = document.createEvent( 'Event' );
+//       event.initEvent( 'DOMContentLoaded', true, true );
+//       window.document.dispatchEvent( event );
+  
+//       expect( document.querySelector( 'p' )
+//           .textContent )
+//         .to.equal( "This is really cool!" );
+//     } )
+  
+//   } )
+  
